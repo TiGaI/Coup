4350139fedee9005ca3a8c5f9d817d9a5161a9b6
Don Dyu <ddkim@connect.ust.hk>
2017-03-02 15:28:12 -0800

    first commit sockets

--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,7 @@
  */
 
 import React, { Component } from 'react';
+
 import {
   AppRegistry,
   StyleSheet,
@@ -12,7 +13,88 @@ import {
   View
 } from 'react-native';
 
+
+
+
+
+var io = require("socket.io-client/socket.io")
+let socket;
+
+
+users = [];
+connections = [];
+
+
+var Test = React.createClass({
+  io.sockets.on('connection', function(socket){
+    connections.push(socket);
+    console.log('Connected: %s sockets connected')
+  })
+})
+
+server.listen(process.env.PORT || 3000)
+//
+// var socketConfig = { path: '/socket' };
+// var socket = new SocketIO('localhost:3000', socketConfig);
+
+// Connect!
+socket.connect();
+
+// An event to be fired on connection to socket
+socket.on('connect', () => {
+    console.log('Wahey -> connected!');
+});
+
+// Event called when 'someEvent' it emitted by server
+socket.on('someEvent', (data) => {
+    console.log('Some event was called, check out this data: ', data);
+});
+
+// Called when anything is emitted by the server
+socket.onAny((event) => {
+    console.log(`${event.name} was called with data: `, event.items);
+});
+
+// Manually join namespace. Ex: namespace is now partyRoom
+socket.joinNamespace('partyRoom')
+
+// Leave namespace, back to '/'
+socket.leaveNamespace()
+
+// Emit an event to server
+socket.emit('helloWorld', {some: 'data'});
+
+//Disconnect from server
+socket.disconnect();
+
+// Reconnect to a closed socket
+socket.reconnect();
+
+
+
+
+var App = React.createClass ({
+  getInitialState: function(){
+    return {
+      createCode: '',
+      joinCode: '',
+      player: 0,
+      navigator: props.navigator
+    }
+  }
+})
+
+
+
 export default class Coup extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
